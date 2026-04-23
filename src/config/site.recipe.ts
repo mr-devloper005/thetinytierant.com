44a3358c046de14f4983c67322f30c9e7c284832
mr-devloper005 <mr.opxoo5@gmail.com>
2026-04-23 18:10:51 +0530

    Configure site tasks and recipe

--- src/config/site.recipe.ts
+++ src/config/site.recipe.ts
@@ -1,19 +1,15 @@
 import type { SiteRecipe } from '@/design/factory/recipe-types'
 
 export const SITE_RECIPE: SiteRecipe = {
-  productFamily: 'directory',
-  themePack: 'yelp-local',
-  homepageTemplate: 'listing-home',
-  navbarTemplate: 'compact-bar',
-  footerTemplate: 'columns-footer',
-  motionPack: 'minimal',
-  primaryTask: 'listing',
-  enabledTasks: ['listing', 'classified', 'profile'],
-  taskTemplates: {
-    listing: 'listing-directory',
-    classified: 'classified-market',
-    profile: 'profile-business',
-  },
+  productFamily: 'visual',
+  themePack: 'pinterest-creator',
+  homepageTemplate: 'image-profile-home',
+  navbarTemplate: 'floating-bar',
+  footerTemplate: 'minimal-footer',
+  motionPack: 'studio-stagger',
+  primaryTask: 'image',
+  enabledTasks: ['image'],
+  taskTemplates: { image: 'image-masonry', profile: 'profile-creator' },
   manualOverrides: {
     navbar: false,
     footer: false,
@@ -26,5 +22,3 @@ export const SITE_RECIPE: SiteRecipe = {
     registerPage: false,
   },
 }
-
-
